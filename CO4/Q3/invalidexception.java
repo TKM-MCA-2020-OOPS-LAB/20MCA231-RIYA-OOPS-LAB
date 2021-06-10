1a309ce309d7b6157cfcb5a2756939c4dfdb9bc9
riyavincent <riyavincent08@gmail.com>
2021-06-10 07:50:10 +0530

    exception handling

---
+++ CO4/Q3/invalidexception.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class invalidexception extends Exception
+{
+    private static final long serialVersionUID = 1L;
+
+ invalidexception(String s)
+ {
+        super(s);
+   }
+    
+}
